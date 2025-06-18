0efedd0506247ff5759a4e971ea54f8dc921de8e
seastj <dev.seastj@gmail.com>
2025-06-18 16:08:33 +0900

    [docs] 타입스크립트 기본자료형

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-const a = "안녕";
+let naverVar1: never = 100;
+let naverVar2: never = true;
+let naverVar3: never = undefined;
+let naverVar4: never = null;
+
+// 전달시 unknown 은 타입오류 발생
+let testNuM:number = neverVar;
