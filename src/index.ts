d566b531d31e57f7097530a2050495b71ad0de78
seastj <dev.seastj@gmail.com>
2025-06-20 12:55:19 +0900

    [docs] 함수 기초

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,43 @@
-type Good = { title: string; price: number; sale: boolean };
+let numA: number = 5;
+let numB: number = 8;
 
-const goodArr: Good[] = [
-  { title: "사과", price: 1000, sale: true },
-  { title: "딸기", price: 5000, sale: false },
-  { title: "메로나", price: 500, sale: true },
-];
-goodArr[0].title;
+const resultPlus: number = numA + numB;
+const resultMinus: number = numA - numB;
+const resultDevide: number = numA / numB;
+const resultMulti: number = numA * numB;
 
-const goodArrGeneric: Array<Good> = [
-  { title: "사과", price: 1000, sale: true },
-  { title: "딸기", price: 5000, sale: false },
-  { title: "메로나", price: 500, sale: true },
-];
-goodArrGeneric[0].title;
+/**
+ * 사칙연산 함수
+ * - 숫자 2개를 입력하시면 결과가 나옵니다.
+ * - 기호는 4가지(+, -, * /)를 사용하실 수 있습니다.
+ *
+ * 사용예시
+ * ```javascript
+ * const result:number = calc(5, 4, "+");
+ * ```
+ * @param {number} a
+ * @param {number} b
+ * @param {string} sign
+ * @returns {number}
+ */
+function calc(a: number, b: number, sign: string): number {
+  let result;
+  switch (sign) {
+    case "+":
+      result = a + b;
+      break;
+    case "-":
+      result = a - b;
+      break;
+    case "*":
+      result = a * b;
+      break;
+    case "/":
+      result = b !== 0 && a !== 0 ? a / b : 0;
+      // 0으로 나누기 방지
+      break;
+  }
+  return result;
+}
+
+const result: number = calc(7, 10, "*");
