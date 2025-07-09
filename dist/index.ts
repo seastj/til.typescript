84b63e0f033e5ebe09df09965c00232371036060
seastj <dev.seastj@gmail.com>
2025-07-09 10:31:38 +0900

    [docs] interface 의 이해

--- dist/index.ts
+++ dist/index.ts
@@ -1,18 +1,27 @@
-type Calculator = {
+interface Animal {
   name: string;
-  add: (a: number, b: number) => number;
-  minus: (a: number, b: number) => number;
-  multi: (a: number, b: number) => number;
-  divide: (a: number, b: number) => number;
+}
+
+interface Dog extends Animal {
+  bark(): void;
+}
+const ani: Dog = {
+  name: "댕댕이",
+  bark: () => console.log("멍멍"),
 };
 
-const calc: Calculator = {
-  name: "계산기",
-  add: (a, b) => a + b,
-  minus: (a, b) => a - b,
-  multi: (a, b) => a * b,
-  divide: (a, b) => a / b,
+interface Cat extends Animal {
+  cry(): void;
+}
+const cat: Cat = {
+  name: "야옹이",
+  cry: () => console.log("야옹"),
 };
 
-calc.name;
-calc.add(5, 6);
+interface Person extends Animal {
+  say(): void;
+}
+const iu: Person = {
+  name: "아이유",
+  say: () => console.log("안녕"),
+};
