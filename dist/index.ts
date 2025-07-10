3f78dc08413786e95b9701352eaaf78c7cc57ebf
seastj <dev.seastj@gmail.com>
2025-07-10 11:31:22 +0900

    [docs] 제네릭의 이해

--- dist/index.ts
+++ dist/index.ts
@@ -1,13 +1,4 @@
-interface 약속 {
-  name: string;
+function mergeArr<T, U>(arr1: T[], arr2: U[]): (T | U)[] {
+  return [...arr1, ...arr2];
 }
-class Person implements 약속 {
-  name: string;
-}
-// 아래도 가능함. 하지만?
-type 약속타입 = {
-  name: string;
-};
-class Dog implements 약속타입 {
-  name: string;
-}
+let result: (string | number)[] = mergeArr([1, 2, 3], ["a", "b", "c"]);
